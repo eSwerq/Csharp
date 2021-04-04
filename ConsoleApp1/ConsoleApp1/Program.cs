da17f0202ead54e3fe0e5a64afe6c4888674759d
eSwerq <75927287+eSwerq@users.noreply.github.com>
2021-04-04 15:28:36 +0600

    gggggg

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = "234526935";
+            for (int i = 0; i < source.Length; i++)
+            {
+                Console.WriteLine(source[i]);
+            }
+        }
+    }
+}
